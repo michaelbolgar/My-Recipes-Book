1535b80a5fc5b6dcfbc22c1fa2ccc44b174d1dce
DmitryLorents <zmey.task@gmail.com>
2023-08-28 22:13:28 +0300

    Refactor code

--- My-Recipes-Book/My-Recipes-Book/Moduls/Home/Controller/HomeViewController.swift
+++ My-Recipes-Book/My-Recipes-Book/Moduls/Home/Controller/HomeViewController.swift
@@ -10,15 +10,14 @@ import SnapKit
 
 class HomeViewController: UIViewController {
     
-    private let homeView = HomeView()
+    private lazy var homeView = {
+        HomeView(frame: view.bounds)
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .cyan
         view.addSubview(homeView)
-        homeView.snp.makeConstraints { make in
-            make.edges.equalToSuperview()
-        }
         
     }
 }
