7da90e8e8fa34e244cb14e8ea9283a0ccf4962bc
Kirilloao <kirilltaraturin747@gmail>
2023-08-28 10:59:12 +0200

    add header to section 0

--- My-Recipes-Book/My-Recipes-Book/Moduls/Recipe detail/Controller/DetailsViewController.swift
+++ My-Recipes-Book/My-Recipes-Book/Moduls/Recipe detail/Controller/DetailsViewController.swift
@@ -7,11 +7,70 @@
 
 import Foundation
 import UIKit
+import SnapKit
 
-class DetailsViewController: UIViewController {
-
+class DetailsViewController: UITableViewController {
+    
+    // MARK: - Life Cycle Methods
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .red
+        view.backgroundColor = .white
+        
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        
     }
+}
+
+extension DetailsViewController {
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        3
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        1
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        
+        cell.backgroundColor = .white
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let headerView = UIView()
+        
+        if section == 0 {
+            let label = UILabel()
+            label.text = "How to make Tasty Fish (point & Kill)"
+            label.font = UIFont.boldSystemFont(ofSize: 27)
+            label.textColor = .black
+            label.numberOfLines = 0
+            label.lineBreakMode = .byWordWrapping
+            
+            headerView.addSubview(label)
+            
+            label.translatesAutoresizingMaskIntoConstraints = false
+            NSLayoutConstraint.activate([
+                label.leadingAnchor.constraint(equalTo: headerView.leadingAnchor, constant: 16),
+                label.trailingAnchor.constraint(equalTo: headerView.trailingAnchor, constant: -16),
+                label.centerYAnchor.constraint(equalTo: headerView.centerYAnchor)
+            ])
+            return headerView
+        } else {
+            return headerView
+        }
+        
+        
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 70 // Укажите желаемую высоту
+    }
+    
+    
+    
+    
+    
+    
 }
