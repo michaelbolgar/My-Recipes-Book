f3bb2626377bd82bb86fa3fd23cc4de29f9b6eec
DmitryLorents <zmey.task@gmail.com>
2023-08-29 21:25:07 +0300

    Move collection view from view to VC

--- My-Recipes-Book/My-Recipes-Book/Moduls/Home/Controller/HomeViewController.swift
+++ My-Recipes-Book/My-Recipes-Book/Moduls/Home/Controller/HomeViewController.swift
@@ -10,15 +10,62 @@ import SnapKit
 
 class HomeViewController: UIViewController {
     
-    private lazy var homeView = {
-        HomeView(frame: view.bounds)
+    //MARK: - Propperties
+    
+    private lazy var collectionView: UICollectionView = {
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: self.createCompositionalLayout() )
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        collectionView.backgroundColor = .yellow
+        return collectionView
     }()
-
+    
+    //    private lazy var homeView = {
+    //        HomeView(frame: view.bounds)
+    //    }()
+    //MARK: - Load view
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .cyan
         title = "Get amazing recepies for cooking"
-        view.addSubview(homeView)
+        //view.addSubview(homeView)
         
     }
+    
+    //MARK: - Methods
+    private func setOutlets() {
+        view.addSubview(collectionView)
+    }
+    
+    private func setupConstraints() {
+        collectionView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
+    }
+    private func createCompositionalLayout() -> UICollectionViewCompositionalLayout {
+        
+        
+    }
+    
+}
+
+
+//MARK: - UICollectionViewDelegate
+
+extension HomeViewController: UICollectionViewDelegate {
+    
+}
+
+//MARK: - UICollectionViewDataSource
+
+extension HomeViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        3
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        UICollectionViewCell()
+    }
+    
+    
 }
