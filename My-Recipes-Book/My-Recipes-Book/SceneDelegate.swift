c0ee708b8097cb3f2ba83d4be6d2bd468576bf25
DmitryLorents <zmey.task@gmail.com>
2023-08-30 22:07:32 +0300

    Return SceneDelegate to initial state

--- My-Recipes-Book/My-Recipes-Book/SceneDelegate.swift
+++ My-Recipes-Book/My-Recipes-Book/SceneDelegate.swift
@@ -16,8 +16,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = (scene as? UIWindowScene) else { return }
 
         let window = UIWindow(windowScene: windowScene)
-        let navVC = UINavigationController(rootViewController: HomeViewController())
-        window.rootViewController = navVC//OnboardingViewController()
+        window.rootViewController = OnboardingViewController()
         window.makeKeyAndVisible()
         self.window = window
     }
