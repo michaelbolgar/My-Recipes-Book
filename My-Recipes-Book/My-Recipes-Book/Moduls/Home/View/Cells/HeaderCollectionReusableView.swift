591add78c55f3c49062d2e37c6627853be783ab4
DmitryLorents <zmey.task@gmail.com>
2023-08-30 16:38:54 +0300

    Add header view

--- My-Recipes-Book/My-Recipes-Book/Moduls/Home/View/Cells/HeaderCollectionReusableView.swift
+++ My-Recipes-Book/My-Recipes-Book/Moduls/Home/View/Cells/HeaderCollectionReusableView.swift
@@ -6,7 +6,25 @@
 //
 
 import UIKit
+import SnapKit
 
 class HeaderCollectionReusableView: UICollectionReusableView {
-        
+   static let reuseID = "HeaderCollectionReusableView"
+    let label = UILabel()
+    
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        label.text = "Trending now 🔥"
+        addSubview(label)
+        label.snp.makeConstraints { make in
+            make.leading.equalToSuperview().inset(16)
+            make.centerY.equalToSuperview()
+        }
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
 }
