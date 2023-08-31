96b567d0cc1c4a4418cd90ef556f8326cb5fa370
KurliBoy <finiqw@gmail.com>
2023-08-31 23:56:34 +0700

    Add tableViewCell

--- My-Recipes-Book/My-Recipes-Book/Moduls/Saved Recipes/Controller/SavedViewController.swift
+++ My-Recipes-Book/My-Recipes-Book/Moduls/Saved Recipes/Controller/SavedViewController.swift
@@ -62,7 +62,7 @@ extension SavedViewController: UITableViewDelegate {
 
 extension SavedViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        1
+        10
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
--- My-Recipes-Book/My-Recipes-Book/Moduls/Saved Recipes/View/SavedTableViewCell.swift
+++ My-Recipes-Book/My-Recipes-Book/Moduls/Saved Recipes/View/SavedTableViewCell.swift
@@ -13,6 +13,49 @@ final class SavedTableViewCell: UITableViewCell {
         String(describing: Self.self)
     }
     
+    private lazy var recipeImageView: UIImageView = {
+        let image = UIImageView()
+        return image
+    }()
+    
+    // MARK: Score with Star image
+    
+    private lazy var recipeScoreView: UIView = {
+        let scoreView = UIView()
+        return scoreView
+    }()
+    
+    private lazy var bookmarkView: UIView = {
+        let bookmarkView = UIView()
+        return bookmarkView
+    }()
+    
+    private lazy var timeLabel: UILabel = {
+        let label = UILabel()
+        return label
+    }()
+    
+    private lazy var recipeLabel: UILabel = {
+        let label = UILabel()
+        return label
+    }()
+    
+    // MARK: Author image with name label
+    
+    private lazy var authorView: UIView = {
+        let view = UIView()
+        return view
+    }()
+    
+    private func confidureUI() {
+        contentView.addSubview(recipeImageView)
+        contentView.addSubview(recipeScoreView)
+        contentView.addSubview(bookmarkView)
+        contentView.addSubview(timeLabel)
+        contentView.addSubview(recipeLabel)
+        contentView.addSubview(authorView)
+    }
+    
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
     }
