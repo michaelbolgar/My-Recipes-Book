d92db5bf2add6e99d01f4602d10ee6dfbf3b1b07
DmitryLorents <zmey.task@gmail.com>
2023-08-31 23:14:31 +0300

    Set my VC

--- My-Recipes-Book/My-Recipes-Book/SceneDelegate.swift
+++ My-Recipes-Book/My-Recipes-Book/SceneDelegate.swift
@@ -16,7 +16,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = (scene as? UIWindowScene) else { return }
 
         let window = UIWindow(windowScene: windowScene)
-        window.rootViewController = OnboardingViewController()
+        window.rootViewController = UINavigationController(rootViewController: HomeViewController())//OnboardingViewController()
         window.makeKeyAndVisible()
         self.window = window
     }
