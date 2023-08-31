5682ae058d410b204896c974baf059a58b39cd7c
DmitryLorents <zmey.task@gmail.com>
2023-08-31 23:13:33 +0300

    Setup category cell

--- My-Recipes-Book/My-Recipes-Book/Moduls/Home/View/Cells/PopularCategoryCollectionViewCell.swift
+++ My-Recipes-Book/My-Recipes-Book/Moduls/Home/View/Cells/PopularCategoryCollectionViewCell.swift
@@ -6,19 +6,51 @@
 //
 
 import UIKit
+import SnapKit
 
 class PopularCategoryCollectionViewCell: UICollectionViewCell {
     static let reuseID = "PopularCategoryCollectionViewCell"
-    
+    override var isSelected: Bool {
+        didSet {
+            label.textColor = isSelected ? .white : Palette.redPrimary20
+            contentView.backgroundColor = isSelected ? Palette.redPrimary50 : .none
+            
+        }
+    }
+    private lazy var label: UILabel = {
+        let label = UILabel()
+        label.font = .poppins(weight: .bold, size: 12)
+        label.textColor = isSelected ? .white : Palette.redPrimary20
+        return label
+    }()
+    //MARK: - Init
     override init(frame: CGRect) {
         super.init(frame: frame)
-        contentView.backgroundColor = .blue
-        contentView.layer.borderColor = UIColor.black.cgColor
-        contentView.layer.borderWidth = 1
-        contentView.layer.cornerRadius = 10
+        print("Frame")
+        setViews()
+        setConstraints()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    //MARK: - Methods
+    func setup(with title: String) {
+        label.text = title
+    }
+    
+    fileprivate func setViews() {
+        print("Set outlets")
+        contentView.layer.cornerRadius = 10
+        contentView.clipsToBounds = true
+        contentView.backgroundColor = isSelected ? Palette.redPrimary50 : .none
+        contentView.addSubview(label)
+    }
+    
+    fileprivate func setConstraints() {
+        label.snp.makeConstraints { make in
+            make.leading.trailing.equalToSuperview().inset(12)
+            make.centerY.equalToSuperview()
+        }
     }
 }
