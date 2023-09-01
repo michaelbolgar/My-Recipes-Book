afa901f6216bbe3318882a335fbe19a1cef4a51a
DmitryLorents <zmey.task@gmail.com>
2023-09-01 16:10:04 +0300

    Finish design of Popular creator

--- My-Recipes-Book/My-Recipes-Book/Moduls/Home/View/Cells/RecentCollectionViewCell.swift
+++ My-Recipes-Book/My-Recipes-Book/Moduls/Home/View/Cells/RecentCollectionViewCell.swift
@@ -10,15 +10,68 @@ import UIKit
 class RecentCollectionViewCell: UICollectionViewCell {
     static let reuseID = "RecentCollectionViewCell"
     
+    private lazy var dishImageView: UIImageView = {
+        let view = UIImageView()
+        view.backgroundColor = .gray
+        view.layer.cornerRadius = 10
+        return view
+    }()
+    
+    private lazy var creatorNameLabel: UILabel = {
+        let label = UILabel()
+        label.text = "By Zeelicious Foods"
+        label.textColor = Palette.neutral30
+        label.font = .poppins(weight: .regular, size: 12)
+        return label
+    }()
+    
+    private lazy var mainLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Kelewele Ghanian Recipe"
+        label.numberOfLines = 2
+        label.adjustsFontSizeToFitWidth = true
+        label.textColor = Palette.neutral100
+        label.font = .poppins(weight: .bold, size: 14)
+        return label
+    }()
+    
+    //MARK: - Init
     override init(frame: CGRect) {
         super.init(frame: frame)
-        contentView.backgroundColor = .cyan
-        contentView.layer.borderColor = UIColor.black.cgColor
-        contentView.layer.borderWidth = 1
-        contentView.layer.cornerRadius = 10
+        setViews()
+        setConstraints()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    //MARK: - Methods
+    private func setViews() {
+        contentView.addSubview(dishImageView)
+        contentView.addSubview(creatorNameLabel)
+        contentView.addSubview(mainLabel)
+    }
+    
+    private func setConstraints() {
+        
+        dishImageView.snp.makeConstraints { make in
+            make.top.leading.trailing.equalToSuperview()
+            make.height.equalTo(dishImageView.snp.width)
+        }
+        
+        mainLabel.snp.makeConstraints { make in
+            make.trailing.leading.equalToSuperview()
+            make.top.equalTo(dishImageView.snp.bottom).inset(-8)
+        }
+        
+        creatorNameLabel.snp.makeConstraints { make in
+            make.leading.trailing.equalToSuperview().inset(3)
+            make.bottom.equalToSuperview()
+        }
+    }
+    
+    func setupCell() {
+        
     }
 }
