7ad321473615681ba9ed3204e5e5a214c8be5ef5
DmitryLorents <zmey.task@gmail.com>
2023-09-01 16:10:17 +0300

    Finish design of recent recipe

--- My-Recipes-Book/My-Recipes-Book/Moduls/Home/View/Cells/PopularCreatorCollectionViewCell.swift
+++ My-Recipes-Book/My-Recipes-Book/Moduls/Home/View/Cells/PopularCreatorCollectionViewCell.swift
@@ -10,15 +10,52 @@ import UIKit
 class PopularCreatorCollectionViewCell: UICollectionViewCell {
     static let reuseID = "PopularCollectionViewCell"
     
+    private lazy var creatorImageView: UIImageView = {
+        let view = UIImageView()
+        view.backgroundColor = .yellow
+        //view.layer.cornerRadius = creatorImageView.frame.height / 2
+        return view
+    }()
+    
+    private lazy var creatorNameLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Mahmud"
+        label.textColor = Palette.neutral100
+        label.textAlignment = .center
+        label.font = .poppins(weight: .bold, size: 12)
+        return label
+    }()
+    
+    //MARK: - Init
     override init(frame: CGRect) {
         super.init(frame: frame)
-        contentView.backgroundColor = .green
-        contentView.layer.borderColor = UIColor.black.cgColor
-        contentView.layer.borderWidth = 1
-        contentView.layer.cornerRadius = 10
+        setViews()
+        setConstraints()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    //MARK: - Methods
+    private func setViews() {
+        contentView.addSubview(creatorImageView)
+        contentView.addSubview(creatorNameLabel)
+    }
+    
+    private func setConstraints() {
+        
+        creatorImageView.snp.makeConstraints { make in
+            make.top.leading.trailing.equalToSuperview()
+            make.height.equalTo(creatorImageView.snp.width)
+        }
+        
+        creatorNameLabel.snp.makeConstraints { make in
+            make.leading.trailing.bottom.equalToSuperview()
+        }
+    }
+    
+    func setupCell() {
+        
     }
 }
