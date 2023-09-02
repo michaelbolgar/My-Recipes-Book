e87674c612f318ca1a69ad69464ffdd03d49d7fd
michaelbolgar <mr.mbolgar@inbox.ru>
2023-09-02 10:13:20 +0200

    created tabbar

---
+++ My-Recipes-Book/My-Recipes-Book/Moduls/Profile/Controller/ProfileViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ProfileViewController.swift
+//  My-Recipes-Book
+//
+//  Created by Михаил Болгар on 02.09.2023.
+//
+
+import Foundation
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+    }
+}
