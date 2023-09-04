2d2337b753e3e78ad37f19e0e161e4b5f0b82f62
DmitryLorents <zmey.task@gmail.com>
2023-09-04 14:29:15 +0300

    Add database model

--- My-Recipes-Book/My-Recipes-Book/Moduls/Home/Model/DataBase.swift
+++ My-Recipes-Book/My-Recipes-Book/Moduls/Home/Model/DataBase.swift
@@ -6,3 +6,21 @@
 //
 
 import Foundation
+import UIKit
+
+class DataBase {
+    static let shared = DataBase()
+    private init() {}
+    
+    let creatorImagesArray = [UIImage(named: "images"),
+                              UIImage(named: "images-2"),
+                              UIImage(named: "images-3"),
+                              UIImage(named: "images-4"),
+                              UIImage(named: "images-5"),
+                              UIImage(named: "images-6"),
+                              UIImage(named: "images-7"),
+                              UIImage(named: "images-8"),
+                              UIImage(named: "images-9"),
+                              UIImage(named: "images-10"),
+                              UIImage(named: "images-11")]
+}
