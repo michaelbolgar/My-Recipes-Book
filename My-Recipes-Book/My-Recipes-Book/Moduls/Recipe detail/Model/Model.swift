09cc70528e3436925871e9b617e199e09d9b1e8d
michaelbolgar <mr.mbolgar@inbox.ru>
2023-09-09 12:42:32 +0200

    added fails manually

--- My-Recipes-Book/My-Recipes-Book/Moduls/Recipe detail/Model/Model.swift
+++ My-Recipes-Book/My-Recipes-Book/Moduls/Recipe detail/Model/Model.swift
@@ -2,17 +2,9 @@
 //  Model.swift
 //  My-Recipes-Book
 //
-//  Created by admin on 08.09.2023.
+//  Created by Михаил Болгар on 09.09.2023.
 //
 
-/*
- Model(recipeImage: "RecipeImage",
-         score: "5.0",
-         time: "15:10",
-         recipeName: "How to make Shawrma at home?",
-         authorImage: "AuthorImage",
-         authorLabel: "By Zeelicious Food")
- */
 import Foundation
 
 struct Model {
